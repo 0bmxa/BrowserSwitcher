39bd3bc442fe2d589b943fc922b1e2a57837b106
0bmxa <0bmxa@users.noreply.github.com>
2019-03-27 16:44:08 +0100

    10.9 and windows fanciness

--- BrowserSwitcher/AppDelegate.swift
+++ BrowserSwitcher/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     /// Handles manual app launches
     @objc func handleAppOpen(event: NSAppleEventDescriptor, replyEvent: NSAppleEventDescriptor) {
         self.appWasLaunchedManually = true
-        self.window.setIsVisible(true)
+        self.showWindow()
     }
     
     /// Quits the app, if it was lanuched from an event
@@ -40,5 +40,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     /// Makes the window visible
     internal func showWindow() {
         self.window.setIsVisible(true)
+        
+        if #available(OSX 10.10, *) {
+            self.removeTitleBar()
+        }
+        if #available(OSX 10.11, *) {
+            self.addTransparency()
+        }
+    }
+}
+
+
+// MARK: - "Window controller"
+extension AppDelegate {
+    
+    @available(OSX 10.10, *)
+    private func removeTitleBar() {
+        guard let window = self.window else { return }
+        
+        window.titlebarAppearsTransparent = true
+        window.titleVisibility = .hidden
+        window.styleMask.insert(.fullSizeContentView)
+    }
+    
+    @available(OSX 10.11, *)
+    private func addTransparency() {
+        guard let contentView = self.window?.contentView else { return }
+        
+        let visualEffect = NSVisualEffectView()
+        visualEffect.blendingMode = .behindWindow
+        visualEffect.state = .active
+        visualEffect.material = .dark
+        
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(visualEffect, positioned: .below, relativeTo: nil)
+        visualEffect.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        visualEffect.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        visualEffect.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        visualEffect.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
 }
